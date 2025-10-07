9ac8fd186f53f02ad7b833eb904877e4885362e9
innotech856 <innocent22222222222@gmail.com>
2025-10-07 21:49:38 +0300

    Update settings.js for SUBZERO-MD

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;PU9UXKRK#S6tnDZ9W2UOoQKsmojH9uDVi-ADGGLLHB1kcd8lfRsE",
+  OWNER_NUMBER: "256700898947", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
